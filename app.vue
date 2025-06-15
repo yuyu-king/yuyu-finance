8334262f4a1389392d5bd0c430b4f549dbe98e37
lootus <44737105+yuyu-king@users.noreply.github.com>
2025-06-15 23:18:25 +0800

    nuxt3 + tailwindcss

---
+++ app.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="bg-red-500">
+    <NuxtRouteAnnouncer />
+    <NuxtWelcome />
+    <h1 class="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  </div>
+</template>
