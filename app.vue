bdede5da3be66e59e00a2bff0495a993b77a6f81
lootus <44737105+yuyu-king@users.noreply.github.com>
2025-06-16 11:51:25 +0800

    清空 master 分支，准备迁移到 Next.js

--- app.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="bg-red-500">
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-    <h1 class="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  </div>
-</template>
